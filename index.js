7fd521d7df8ee272ccc75bb05bf7b0992d77940a
tenmakamatani <tenmakamatani0@gmail.com>
2019-08-16 00:01:49 +0900

    [update]リクエストを検知し、送信

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const config = {
     channelSecret: process.env.SECRET_KEY
 };
 
+const client = new line.Client(config);
+
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
@@ -21,8 +23,25 @@ app.get("/", (req, res) => {
 
 app.post("/hook", line.middleware(config), (req, res) => {
     res.status(200).end();
-    console.log("passed");
+
+    const events = req.body.events;
+    const promises = [];
+    for (let i = 0; i < events.length; i++) {
+        const ev = events[i];
+        promises.push(
+            echoman(ev)
+        );
+    }
+    Promise.all(promises).then(console.log("pass"));
 });
+
+async function echoman(ev) {
+    const pro = await client.getProfile(ev.source.userId);
+    return client.replyMessage(ev.replyToken, {
+        type: "text",
+        text: `${pro.displayName}さん、今「${ev.message.text}」って言いましたね？`
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
