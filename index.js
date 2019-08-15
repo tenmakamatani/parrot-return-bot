f401358e2e692777ff6e6b0c43f16acb3c097331
tenmakamatani <tenmakamatani0@gmail.com>
2019-08-15 23:22:47 +0900

    [update]雛形追加

---
+++ index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 8000;
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+    res.send("ようこそオウム返しBOTヘ");
+});
+
+app.post("/hook", (req, res) => {
+    res.json({
+        test: "hook"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`)
+});
